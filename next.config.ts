2b9994309ac6699d6233449e70784ad92454f422
Aiman <apple@192.168.1.9>
2025-12-16 21:38:01 +0530

    Additional config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ["@sparticuz/chromium"],
+  serverExternalPackages: ["@sparticuz/chromium", "playwright-core"],
+    
+  experimental: {
+      serverActions: {
+          allowedOrigins: ['*']
+      }
+  }
 };
 
 export default nextConfig;
